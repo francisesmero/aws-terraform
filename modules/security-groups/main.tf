5d193360599337a104e6dab8788b27abc653981f
Francis Esmero <francis.esmero@gmail.com>
2024-04-04 23:19:42 +0800

    chore: refactor vpc module

--- modules/security-groups/main.tf
+++ modules/security-groups/main.tf
@@ -1,78 +1,17 @@
 resource "aws_security_group" "bastion_security_group" {
-    name                        = "bastion security group"
+    name                        = "awsdemo-bastion-sg"
     description                 = "enable internet access to Bastion Host"
     vpc_id                      = var.vpc_id
-
-    ingress {
-        description             = "allow ssh access"
-        from_port               = 22
-        to_port                 = 22
-        protocol                = "tcp"
-        cidr_blocks             = ["0.0.0.0/0"]
-    }
-
-    # egress {
-    #     description             = "Allow access from bastion to database"
-    #     from_port               = 3306
-    #     to_port                 = 3306
-    #     protocol                = "tcp"
-    #     cidr_blocks             = ["0.0.0.0/0"]
-    # }
-
-
-    # egress {
-    #     description             = "Allow http access from bastion to web-server"
-    #     from_port               = 80
-    #     to_port                 = 80
-    #     protocol                = "tcp"
-    #     cidr_blocks             = ["0.0.0.0/0"]
-    # }
-
-    #   egress {
-    #     description             = "Allow https access from bastion to web-server"
-    #     from_port               = 443
-    #     to_port                 = 443
-    #     protocol                = "tcp"
-    #     cidr_blocks             = ["0.0.0.0/0"]
-    # }
-    
-
-    # egress {
-    #     description             = "https access to web-server"
-    #     from_port               = 443
-    #     to_port                 = 443 
-    #     protocol                = "tcp"
-    #     security_groups         = [aws_security_group.bastion_security_group.id]
-    # }
-
 }
 
-# resource "aws_security_group" "backend_security_group" {
-#     ingress {
-#         from_port       = var.port_number
-#         to_port         = var.port_number
-#         protocol        = var.protocol_type
-#         cidr_blocks     = var.cidr_source
-#     }
+# resource "aws_security_group_rule" "bastion_ingress_rule" {
+#     type                        = "ingress"
+#     from_port                   = "22"
+#     to_port                     = "22"
+#     protocol_type               = "tcp"
+#     cidr_blocks                 = aws_vpc.vpc.cidr_block
+#     security_group_id           = aws_security_group.bastion_security_group.id
 
-#     egress {
-#         from_port       = var.port_number
-#         to_port         = var.port_number
-#         protocol        = var.protocol_type
-#         cidr_blocks     = var.cidr_source
-#     }
 # }
 
-# resource "aws_security_group" "frontend_security_group" {
-#     ingress:
-#         internet -> frontend
-#     egress:
-#         frontend -> backend
-# }
 
-# resource "aws_security_group" "rds_security_group"{
-#     ingress:
-#         backend -> rds
-#     egress:
-#         rds -> backend
-# }
--- modules/vpc/main.tf
+++ modules/vpc/main.tf
@@ -1,39 +1,68 @@
 resource "aws_vpc" "vpc" {
-  cidr_block           = var.cidr_block
+  cidr_block           = var.vpc_cidr_block
+  instance_tenancy     = "default"
   enable_dns_support   = true
 
   tags = {
-    Name = "awsdemo-vpc"
+    Name = "${var.project_name}-vpc"
   }
 }
 
 resource "aws_internet_gateway" "my_internet_gw" {
-  vpc_id              = aws_vpc.vpc.id
+  vpc_id                      = aws_vpc.vpc.id
 
   tags = {
-    Name              = "awsdemo-igw"
+    Name                      = "${var.project_name}-igw"
+  }
+}
+
+data "availability_zones" "available_zones" {}
+
+
+resource "aws_subnet" "public_subnet_az1" {
+  vpc_id                      = aws_vpc.vpc.id
+  cidr_block                  = var.public_subnet_az1_cidr_block
+  availability_zone           = data.aws_availability_zones.available_zones.names[0]
+  map_public_ip_on_launch     = true
+
+  tags = {
+   Name = "${var.project_name}-public-subnet-az1"
+  }
+}
+
+resource "aws_subnet" "public_subnet_az2" {
+  vpc_id                      = aws_vpc.vpc.id
+  cidr_block                  = var.public_subnet_az2_cidr_block
+  availability_zone           = data.aws_availability_zones.available_zones.names[1]
+  map_public_ip_on_launch     = true
+
+  tags = {
+   Name = "${var.project_name}-public-subnet-az2"
+  }
+}
+
+resource "aws_subnet" "private_subnet_az1" {
+  vpc_id                      = aws_vpc.vpc.id
+  cidr_block                  = var.private_subnet_az1_cidr_block
+  availability_zone           = data.aws_availability_zones.available_zones.names[0]
+  map_public_ip_on_launch     = true
+
+  tags = {
+   Name = "${var.project_name}-public-subnet-az1"
   }
 }
 
 
-resource "aws_subnet" "public" {
-  count               = var.num_availability_zones
-  vpc_id              = aws_vpc.vpc.id
-  cidr_block          = cidrsubnet(var.cidr_block, 8, count.index + 1)
-  availability_zone   = var.availability_zones[count.index]
+resource "aws_subnet" "private_subnet_az2" {
+  vpc_id                      = aws_vpc.vpc.id
+  cidr_block                  = var.private_subnet_az2_cidr_block
+  availability_zone           = data.aws_availability_zones.available_zones.names[1]
+  map_public_ip_on_launch     = true
 
   tags = {
-    Name = format("awsdemo-public-subnet-%d", count.index + 1)
+   Name = "${var.project_name}-public-subnet-az1"
   }
 }
 
-resource "aws_subnet" "private" {
-  count               = var.num_availability_zones
-  vpc_id              = aws_vpc.vpc.id
-  cidr_block          = cidrsubnet(var.cidr_block, 8, count.index + 3)
-  availability_zone   = var.availability_zones[count.index]
 
-  tags = {
-    Name = format("awsdemo-private-subnet-%d", count.index +1)
-  }
-}
+
