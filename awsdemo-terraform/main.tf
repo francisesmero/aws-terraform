94910e2e11b658d00f094d8f0d0a6bf965b4385f
Francis Esmero <francis.esmero@gmail.com>
2024-04-05 15:10:08 +0800

    feature: store values in tfvars

---
+++ awsdemo-terraform/main.tf
@@ -0,0 +1,11 @@
+module "vpc" {
+  source                                = "../modules/vpc"
+  availability_zones                    = var.availability_zones
+  region                                = var.region
+  project_name                          = var.project_name
+  vpc_cidr_block                        = var.vpc_cidr_block
+  public_subnet_az1_cidr_block          = var.public_subnet_az1_cidr_block
+  public_subnet_az2_cidr_block          = var.public_subnet_az2_cidr_block
+  private_subnet_az1_cidr_block         = var.private_subnet_az1_cidr_block
+  private_subnet_az2_cidr_block         = var.private_subnet_az2_cidr_block
+}
